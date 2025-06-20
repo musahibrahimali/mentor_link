05cf5383b98f3042b2dfa45d042755563a42dc99
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2025-06-20 15:26:13 +0100

    making changes

---
+++ src/components/features/othres/others.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Others = () => {
+    return (
+        <div>
+        <h2>Others</h2>
+        <p>This is the others section.</p>
+        </div>
+    );
+};
+
+export default Others;
